73acf6a0ed36dd53899486cc787842747f575eec
erikagodin6 <54984224-erikagodin6@users.noreply.replit.com>
2026-03-13 21:19:04 +0000

    Merged changes from mwqm4srl/main
    Replit-Task-Id: 92947a7e-d27e-481f-aceb-b48b772a4b4d

--- shared/tier-counts.ts
+++ shared/tier-counts.ts
@@ -5,5 +5,5 @@ export const tierCounts = {
     np: 2174,
     totalStatic: 6078,
     questionCount: 0,
-    computedAt: "2026-03-13T21:09:51.666Z",
+    computedAt: "2026-03-13T21:18:37.372Z",
   };
