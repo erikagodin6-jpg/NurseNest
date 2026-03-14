63d65959089de2c5257e57803279625191875573
erikagodin6 <54984224-erikagodin6@users.noreply.replit.com>
2026-03-14 01:06:22 +0000

    Git commit prior to merge

--- shared/tier-counts.ts
+++ shared/tier-counts.ts
@@ -5,5 +5,5 @@ export const tierCounts = {
     np: 2174,
     totalStatic: 6078,
     questionCount: 0,
-    computedAt: "2026-03-14T01:05:53.245Z",
+    computedAt: "2026-03-14T01:06:21.260Z",
   };
