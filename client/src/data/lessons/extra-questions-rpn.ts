1315ad608ccc3f40b3054ba2207fb6804a20debb
erikagodin6-jpg <erikagodin6@gmail.com>
2026-08-07 16:47:12 -0400

    content(rexpn): wire batches 050 through 053 into lesson map

--- client/src/data/lessons/extra-questions-rpn.ts
+++ client/src/data/lessons/extra-questions-rpn.ts
@@ -5,6 +5,10 @@ import { rpnContentBatch046Lessons } from "./rpn-content-batch-046";
 import { rpnContentBatch047Lessons } from "./rpn-content-batch-047";
 import { rpnContentBatch048Lessons } from "./rpn-content-batch-048";
 import { rpnContentBatch049Lessons } from "./rpn-content-batch-049";
+import { rpnContentBatch050Lessons } from "./rpn-content-batch-050";
+import { rpnContentBatch051Lessons } from "./rpn-content-batch-051";
+import { rpnContentBatch052Lessons } from "./rpn-content-batch-052";
+import { rpnContentBatch053Lessons } from "./rpn-content-batch-053";
 
 /**
  * REx-PN / Canadian practical-nursing expansion aggregator.
@@ -17,4 +21,8 @@ export const rpnExtraBank: Record<string, LessonContent> = {
   ...rpnContentBatch047Lessons,
   ...rpnContentBatch048Lessons,
   ...rpnContentBatch049Lessons,
+  ...rpnContentBatch050Lessons,
+  ...rpnContentBatch051Lessons,
+  ...rpnContentBatch052Lessons,
+  ...rpnContentBatch053Lessons,
 };
