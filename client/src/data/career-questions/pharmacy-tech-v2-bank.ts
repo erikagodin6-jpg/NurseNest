bca2f24ed9762adf176b9d0e464c7572344c7f41
erikagodin6-jpg <erikagodin6@gmail.com>
2026-08-08 23:26:40 -0400

    Add pharmacy technician calculations batch 02

--- client/src/data/career-questions/pharmacy-tech-v2-bank.ts
+++ client/src/data/career-questions/pharmacy-tech-v2-bank.ts
@@ -1,5 +1,7 @@
 import { pharmacyTechV2CalculationsBatch01Questions } from "./pharmacy-tech-v2-calculations-batch01-questions";
+import { pharmacyTechV2CalculationsBatch02Questions } from "./pharmacy-tech-v2-calculations-batch02-questions";
 
 export const PHARMACY_TECH_V2_QUESTIONS:any[]=[
   ...pharmacyTechV2CalculationsBatch01Questions,
+  ...pharmacyTechV2CalculationsBatch02Questions,
 ];
