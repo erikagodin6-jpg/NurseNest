3cafd58e28b649e773795013726f19a4943a23e5
erikagodin6-jpg <erikagodin6@gmail.com>
2026-08-08 23:28:31 -0400

    Add pharmacy technician compounding batch 03

--- client/src/data/career-questions/pharmacy-tech-v2-bank.ts
+++ client/src/data/career-questions/pharmacy-tech-v2-bank.ts
@@ -1,7 +1,9 @@
 import { pharmacyTechV2CalculationsBatch01Questions } from "./pharmacy-tech-v2-calculations-batch01-questions";
 import { pharmacyTechV2CalculationsBatch02Questions } from "./pharmacy-tech-v2-calculations-batch02-questions";
+import { pharmacyTechV2CompoundingBatch03Questions } from "./pharmacy-tech-v2-compounding-batch03-questions";
 
 export const PHARMACY_TECH_V2_QUESTIONS:any[]=[
   ...pharmacyTechV2CalculationsBatch01Questions,
   ...pharmacyTechV2CalculationsBatch02Questions,
+  ...pharmacyTechV2CompoundingBatch03Questions,
 ];
