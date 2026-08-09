f3f3dfdd50842a17dae613c575f524b550683934
erikagodin6-jpg <erikagodin6@gmail.com>
2026-08-08 23:24:48 -0400

    Create pharmacy technician v2 bank

---
+++ client/src/data/career-questions/pharmacy-tech-v2-bank.ts
@@ -0,0 +1,5 @@
+import { pharmacyTechV2CalculationsBatch01Questions } from "./pharmacy-tech-v2-calculations-batch01-questions";
+
+export const PHARMACY_TECH_V2_QUESTIONS:any[]=[
+  ...pharmacyTechV2CalculationsBatch01Questions,
+];
