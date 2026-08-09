89aef52b4ef4346e4086ef455de5575c4e122542
erikagodin6-jpg <erikagodin6@gmail.com>
2026-08-08 22:45:50 -0400

    Aggregate authored paramedic v2 batches

---
+++ client/src/data/career-questions/paramedic-v2-bank.ts
@@ -0,0 +1,9 @@
+import { paramedicV2Questions } from "./paramedic-v2-questions";
+import { paramedicV2TraumaBatch02Questions } from "./paramedic-v2-trauma-batch02-questions";
+import { paramedicV2AclsBurnBatch03Questions } from "./paramedic-v2-acls-burn-batch03-questions";
+
+export const PARAMEDIC_V2_QUESTIONS:any[]=[
+  ...paramedicV2Questions,
+  ...paramedicV2TraumaBatch02Questions,
+  ...paramedicV2AclsBurnBatch03Questions,
+];
