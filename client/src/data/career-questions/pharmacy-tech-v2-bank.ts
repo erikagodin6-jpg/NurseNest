b701a04f28a2f5c04abef4840e506d325692572e
erikagodin6-jpg <erikagodin6@gmail.com>
2026-08-08 23:31:39 -0400

    Add pharmacy technician law batch 05

--- client/src/data/career-questions/pharmacy-tech-v2-bank.ts
+++ client/src/data/career-questions/pharmacy-tech-v2-bank.ts
@@ -2,10 +2,12 @@ import { pharmacyTechV2CalculationsBatch01Questions } from "./pharmacy-tech-v2-c
 import { pharmacyTechV2CalculationsBatch02Questions } from "./pharmacy-tech-v2-calculations-batch02-questions";
 import { pharmacyTechV2CompoundingBatch03Questions } from "./pharmacy-tech-v2-compounding-batch03-questions";
 import { pharmacyTechV2MedicationsBatch04Questions } from "./pharmacy-tech-v2-medications-batch04-questions";
+import { pharmacyTechV2LawBatch05Questions } from "./pharmacy-tech-v2-law-batch05-questions";
 
 export const PHARMACY_TECH_V2_QUESTIONS:any[]=[
   ...pharmacyTechV2CalculationsBatch01Questions,
   ...pharmacyTechV2CalculationsBatch02Questions,
   ...pharmacyTechV2CompoundingBatch03Questions,
   ...pharmacyTechV2MedicationsBatch04Questions,
+  ...pharmacyTechV2LawBatch05Questions,
 ];
