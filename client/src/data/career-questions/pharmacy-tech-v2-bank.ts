5778f232f6c94ca5931872e8f75f9373f8bc85c7
erikagodin6-jpg <erikagodin6@gmail.com>
2026-08-08 23:29:58 -0400

    Add pharmacy technician medications batch 04

--- client/src/data/career-questions/pharmacy-tech-v2-bank.ts
+++ client/src/data/career-questions/pharmacy-tech-v2-bank.ts
@@ -1,9 +1,11 @@
 import { pharmacyTechV2CalculationsBatch01Questions } from "./pharmacy-tech-v2-calculations-batch01-questions";
 import { pharmacyTechV2CalculationsBatch02Questions } from "./pharmacy-tech-v2-calculations-batch02-questions";
 import { pharmacyTechV2CompoundingBatch03Questions } from "./pharmacy-tech-v2-compounding-batch03-questions";
+import { pharmacyTechV2MedicationsBatch04Questions } from "./pharmacy-tech-v2-medications-batch04-questions";
 
 export const PHARMACY_TECH_V2_QUESTIONS:any[]=[
   ...pharmacyTechV2CalculationsBatch01Questions,
   ...pharmacyTechV2CalculationsBatch02Questions,
   ...pharmacyTechV2CompoundingBatch03Questions,
+  ...pharmacyTechV2MedicationsBatch04Questions,
 ];
